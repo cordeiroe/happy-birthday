3e1f2989dd05c51d8a153b55adbeeceb6431e3e3
Emerson <emersoncmarques@outlook.com>
2024-03-23 00:55:12 -0300

    wip

--- js/index.js
+++ js/index.js
@@ -7,21 +7,29 @@
       openB = $('open'),
       closeB = $('close'),
       timer = null;
-  console.log('wat', card);
+
   openB.addEventListener('click', function () {
     card.setAttribute('class', 'open-half');
     if (timer) clearTimeout(timer);
     timer = setTimeout(function () {
       card.setAttribute('class', 'open-fully');
+      $('page1').setAttribute('class', 'visible');
+      $('page2').setAttribute('class', 'hidden');
+      $('page3').setAttribute('class', 'visible');
+      $('page4').setAttribute('class', 'hidden');
       timer = null;
     }, 1000);
   });
 
   closeB.addEventListener('click', function () {
     card.setAttribute('class', 'close-half');
-    if (timer) clearTimerout(timer);
+    if (timer) clearTimeout(timer);
     timer = setTimeout(function () {
       card.setAttribute('class', '');
+      $('page1').setAttribute('class', 'hidden');
+      $('page2').setAttribute('class', 'visible');
+      $('page3').setAttribute('class', 'hidden');
+      $('page4').setAttribute('class', 'visible');
       timer = null;
     }, 1000);
   });
